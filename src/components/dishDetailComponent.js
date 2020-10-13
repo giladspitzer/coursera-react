f514865cf8d80968113cdda6ba8e9053fe68c018
Gilad Spitzer <giladspitzer@gmail.com>
2020-10-13 12:15:53 +0300

    redux thunk completed

--- src/components/dishDetailComponent.js
+++ src/components/dishDetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, BreadcrumbItem, Breadcrum
 Modal, ModalBody, ModalHeader, Label, Button, Row, Col } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import { Control, LocalForm, Errors} from 'react-redux-form'
+import { Loading } from './loadingComponent';
 
 
 
@@ -139,7 +140,23 @@ const DishDetail = (props) => {
     console.log('DishDetail Component render is invoked')
     const dish= props.dish
     const comments = props.comments
-    if (dish != null){
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    } else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    } else if (props.dish != null) {
         return(
             <div className="container">
                 <Breadcrumb>
